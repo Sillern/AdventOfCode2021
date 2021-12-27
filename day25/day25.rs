d61ed1262aa4127248b61f2947fa7ec4810d8850
Sebastian Jegerås <jegeras@gmail.com>
2021-12-28 00:39:26 +0100

    Solved day25 part1

--- day25/day25.rs
+++ day25/day25.rs
@@ -6,12 +6,13 @@ use std::collections::HashMap;
 use std::env;
 
 type Coordinate = (i32, i32);
-type Map = HashMap<Coordinate, SeacucumberDirection>;
+type Map = HashMap<Coordinate, MapType>;
 
-#[derive(Debug)]
-enum SeacucumberDirection {
-    East,
-    South,
+#[derive(Debug, PartialEq, Clone)]
+enum MapType {
+    Empty,
+    MovingEast,
+    MovingSouth,
 }
 
 fn map_from_file(inputfile: &str) -> Map {
@@ -27,11 +28,14 @@ fn map_from_string(input: &str) -> Map {
         row.chars().enumerate().for_each(|(x, c)| {
             let position: Coordinate = (x as i32, y as i32);
             match c {
+                '.' => {
+                    map.insert(position, MapType::Empty);
+                }
                 '>' => {
-                    map.insert(position, SeacucumberDirection::East);
+                    map.insert(position, MapType::MovingEast);
                 }
                 'v' => {
-                    map.insert(position, SeacucumberDirection::South);
+                    map.insert(position, MapType::MovingSouth);
                 }
                 _ => {}
             };
@@ -41,21 +45,80 @@ fn map_from_string(input: &str) -> Map {
     map
 }
 
-fn solve_part1(inputfile: String) -> usize {
-    let mut map = map_from_string(
-        "v...>>.vv>
-.vv>>.vv..
->>.>v>...v
->>v>>.>.v.
-v>v.vv.v..
->.>>..v...
-.vv..>.>v.
-v.v..>>v.v
-....v..v.>",
-    );
+fn print_map(map: &HashMap<Coordinate, MapType>) {
+    let x_min = map.iter().map(|(pos, _)| pos.0).min().unwrap();
+    let x_max = map.iter().map(|(pos, _)| pos.0).max().unwrap();
+    let y_min = map.iter().map(|(pos, _)| pos.1).min().unwrap();
+    let y_max = map.iter().map(|(pos, _)| pos.1).max().unwrap();
 
-    println!("{:?}", map);
-    0
+    println!(
+        "{}",
+        (y_min..y_max + 1)
+            .map(|y| {
+                (x_min..x_max + 1)
+                    .map(|x| {
+                        let position = (x, y);
+                        match map.get(&position) {
+                            Some(MapType::Empty) => ".",
+                            Some(MapType::MovingEast) => ">",
+                            Some(MapType::MovingSouth) => "v",
+                            None => ".",
+                        }
+                    })
+                    .join("")
+            })
+            .join("\n")
+    );
+}
+
+fn step(map: &mut Map) -> usize {
+    let x_max = map.iter().map(|(pos, _)| pos.0).max().unwrap() + 1;
+    let y_max = map.iter().map(|(pos, _)| pos.1).max().unwrap() + 1;
+
+    let mut num_moves = 0;
+
+    for (check_direction, offset) in [
+        (MapType::MovingEast, (1, 0)),
+        (MapType::MovingSouth, (0, 1)),
+    ] {
+        let mut moves: Vec<(Coordinate, Coordinate)> = vec![];
+        for (position, direction) in map
+            .iter()
+            .filter(|&(position, direction)| *direction == check_direction)
+        {
+            let neighbour = (
+                (position.0 + offset.0) % x_max,
+                (position.1 + offset.1) % y_max,
+            );
+
+            if let Some(MapType::Empty) = map.get(&neighbour) {
+                moves.push((*position, neighbour));
+            }
+        }
+
+        num_moves += moves.len();
+
+        for (from, to) in moves {
+            if let Some(direction) = map.get(&from) {
+                map.insert(to, direction.clone());
+                map.entry(from).and_modify(|e| *e = MapType::Empty);
+            }
+        }
+    }
+
+    num_moves
+}
+
+fn solve_part1(inputfile: String) -> usize {
+    let mut map = map_from_file(&inputfile);
+
+    println!();
+    let mut iteration = 0;
+    while step(&mut map) != 0 {
+        println!("steps: {}", iteration);
+        iteration += 1;
+    }
+    iteration + 1
 }
 
 fn solve_part2(inputfile: String) -> usize {
