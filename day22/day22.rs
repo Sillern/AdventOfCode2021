77bb5b8d5deac2bf4c0a32b1ae93ffc5942969b3
Sebastian Jegerås <jegeras@gmail.com>
2022-01-02 19:46:00 +0100

    Solved day22 part2

--- day22/day22.rs
+++ day22/day22.rs
@@ -1,11 +1,11 @@
+use itertools::Itertools;
 use regex::Regex;
-
 use std::collections::HashMap;
 use std::env;
 
 type Coordinate = (i32, i32, i32);
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cuboid {
     status: bool,
     x_range: std::ops::Range<i32>,
@@ -14,6 +14,19 @@ struct Cuboid {
 }
 
 impl Cuboid {
+    pub fn new(
+        status: bool,
+        x_range: std::ops::Range<i32>,
+        y_range: std::ops::Range<i32>,
+        z_range: std::ops::Range<i32>,
+    ) -> Self {
+        Self {
+            status,
+            x_range,
+            y_range,
+            z_range,
+        }
+    }
     pub fn from_string(input: &str) -> Self {
         let re = Regex::new(r"(?P<status>on|off) x=(?P<x_start>[-0-9]+)..(?P<x_stop>[-0-9]+),y=(?P<y_start>[-0-9]+)..(?P<y_stop>[-0-9]+),z=(?P<z_start>[-0-9]+)..(?P<z_stop>[-0-9]+)").unwrap();
         let parsed = re.captures(input).unwrap();
@@ -38,11 +51,27 @@ impl Cuboid {
             z_range,
         }
     }
+
+    pub fn is_empty(&self) -> bool {
+        self.x_range.start >= self.x_range.end
+            || self.y_range.start >= self.y_range.end
+            || self.z_range.start >= self.z_range.end
+    }
+
+    pub fn is_out_of_range(&self) -> bool {
+        let boundary = 50;
+        self.x_range.start.abs() > boundary
+            || self.x_range.end.abs() > boundary
+            || self.y_range.start.abs() > boundary
+            || self.y_range.end.abs() > boundary
+            || self.z_range.start.abs() > boundary
+            || self.z_range.end.abs() > boundary
+    }
 }
 
 #[derive(Debug)]
 struct Reactor {
-    reboot_instructions: Vec<Cuboid>,
+    cuboids: Vec<Cuboid>,
     reboot_index: usize,
     cubes: HashMap<Coordinate, bool>,
 }
@@ -55,7 +84,7 @@ impl Reactor {
     }
     pub fn from_string(input: &str) -> Self {
         Self {
-            reboot_instructions: input
+            cuboids: input
                 .lines()
                 .map(Cuboid::from_string)
                 .collect::<Vec<Cuboid>>(),
@@ -64,41 +93,187 @@ impl Reactor {
         }
     }
 
-    pub fn split_cuboids(&mut self) {
-        for instruction in &self.reboot_instructions {
-            if !instruction.status {
-                println!("detect disjunction: {:?}", &instruction);
-            }
-        }
+    pub fn num_cuboids(&self) -> usize {
+        self.cuboids.len()
     }
 
-    pub fn num_cubes_in_cuboids(&self) -> usize {
-        self.reboot_instructions.iter().fold(0, |acc, instruction| {
-            acc + (instruction.x_range.end - instruction.x_range.start).abs() as usize
-                + (instruction.y_range.end - instruction.y_range.start).abs() as usize
-                + (instruction.z_range.end - instruction.z_range.start).abs() as usize
+    pub fn split_cuboids(&mut self, limited_range: bool) -> Vec<Cuboid> {
+        fn overlaps(a: &Cuboid, b: &Cuboid) -> bool {
+            !(b.x_range.start > a.x_range.end
+                || b.x_range.end < a.x_range.start
+                || b.y_range.start > a.y_range.end
+                || b.y_range.end < a.y_range.start
+                || b.z_range.start > a.z_range.end
+                || b.z_range.end < a.z_range.start)
+        }
+
+        fn subtract(a: &Cuboid, b: &Cuboid) -> Vec<Cuboid> {
+            if !overlaps(a, b) {
+                vec![a.clone()]
+            } else {
+                [
+                    Cuboid::new(
+                        true,
+                        a.x_range.start..b.x_range.start,
+                        a.y_range.clone(),
+                        a.z_range.clone(),
+                    ),
+                    Cuboid::new(
+                        true,
+                        b.x_range.end..a.x_range.end,
+                        a.y_range.clone(),
+                        a.z_range.clone(),
+                    ),
+                    Cuboid::new(
+                        true,
+                        a.x_range.start.max(b.x_range.start)..a.x_range.end.min(b.x_range.end),
+                        a.y_range.start..b.y_range.start,
+                        a.z_range.clone(),
+                    ),
+                    Cuboid::new(
+                        true,
+                        a.x_range.start.max(b.x_range.start)..a.x_range.end.min(b.x_range.end),
+                        b.y_range.end..a.y_range.end,
+                        a.z_range.clone(),
+                    ),
+                    Cuboid::new(
+                        true,
+                        a.x_range.start.max(b.x_range.start)..a.x_range.end.min(b.x_range.end),
+                        a.y_range.start.max(b.y_range.start)..a.y_range.end.min(b.y_range.end),
+                        a.z_range.start..b.z_range.start,
+                    ),
+                    Cuboid::new(
+                        true,
+                        a.x_range.start.max(b.x_range.start)..a.x_range.end.min(b.x_range.end),
+                        a.y_range.start.max(b.y_range.start)..a.y_range.end.min(b.y_range.end),
+                        b.z_range.end..a.z_range.end,
+                    ),
+                ]
+                .into_iter()
+                .filter(|c| !c.is_empty())
+                .collect()
+            }
+        }
+
+        let mut active_cuboids: Vec<Cuboid> = Vec::new();
+
+        for cuboid in &self.cuboids {
+            if limited_range && cuboid.is_out_of_range() {
+                println!("Skipping: {:?}", &cuboid);
+                continue;
+            }
+
+            if cuboid.status {
+                let mut cuts = vec![cuboid.clone()];
+
+                for active_cuboid in active_cuboids.iter() {
+                    let mut new_cuts = Vec::new();
+
+                    for cut in cuts {
+                        new_cuts.extend(subtract(&cut, &active_cuboid));
+                    }
+
+                    cuts = new_cuts;
+                }
+
+                active_cuboids.extend(cuts);
+            } else {
+                let mut cuts = Vec::new();
+
+                for active_cuboid in active_cuboids.into_iter() {
+                    cuts.extend(subtract(&active_cuboid, &cuboid));
+                }
+
+                active_cuboids = cuts;
+            }
+        }
+        active_cuboids
+    }
+
+    pub fn num_cubes_in_cuboids(cuboids: &Vec<Cuboid>) -> usize {
+        cuboids.iter().fold(0, |acc, cuboid| {
+            let x_range = (cuboid.x_range.end - cuboid.x_range.start).abs() as usize;
+            let y_range = (cuboid.y_range.end - cuboid.y_range.start).abs() as usize;
+            let z_range = (cuboid.z_range.end - cuboid.z_range.start).abs() as usize;
+
+            acc + (x_range * y_range * z_range)
         })
+    }
+
+    pub fn print(cuboids: &Vec<Cuboid>) {
+        let x_min = cuboids
+            .iter()
+            .map(|cuboid| cuboid.x_range.start)
+            .min()
+            .unwrap();
+        let x_max = cuboids
+            .iter()
+            .map(|cuboid| cuboid.x_range.end)
+            .max()
+            .unwrap();
+        let y_min = cuboids
+            .iter()
+            .map(|cuboid| cuboid.y_range.start)
+            .min()
+            .unwrap();
+        let y_max = cuboids
+            .iter()
+            .map(|cuboid| cuboid.y_range.end)
+            .max()
+            .unwrap();
+
+        println!(
+            "{}",
+            (y_min..y_max + 1)
+                .map(|y| {
+                    (x_min..x_max + 1)
+                        .map(|x| {
+                            if let Some((status, index)) = cuboids
+                                .iter()
+                                .enumerate()
+                                .filter_map(|(index, cuboid)| {
+                                    if cuboid.x_range.contains(&x) && cuboid.y_range.contains(&y) {
+                                        Some((cuboid.status, index))
+                                    } else {
+                                        None
+                                    }
+                                })
+                                .last()
+                            {
+                                if status {
+                                    format!("{}", index)
+                                } else {
+                                    ".".to_string()
+                                }
+                            } else {
+                                " ".to_string()
+                            }
+                        })
+                        .join("")
+                })
+                .join("\n")
+        );
     }
 
     pub fn limited_boot(&mut self) {
         let boundary = 50;
-        for instruction in &self.reboot_instructions {
-            for z in instruction.z_range.clone() {
+        for cuboid in &self.cuboids {
+            for z in cuboid.z_range.clone() {
                 if z.abs() > boundary {
                     continue;
                 }
-                for y in instruction.y_range.clone() {
+                for y in cuboid.y_range.clone() {
                     if y.abs() > boundary {
                         continue;
                     }
-                    for x in instruction.x_range.clone() {
+                    for x in cuboid.x_range.clone() {
                         if x.abs() > boundary {
                             continue;
                         }
                         self.cubes
                             .entry((x, y, z))
-                            .and_modify(|status| *status = instruction.status)
-                            .or_insert(instruction.status);
+                            .and_modify(|status| *status = cuboid.status)
+                            .or_insert(cuboid.status);
                     }
                 }
             }
@@ -108,15 +283,15 @@ impl Reactor {
     }
 
     pub fn step(&mut self) {
-        let instruction = &self.reboot_instructions[self.reboot_index];
+        let cuboid = &self.cuboids[self.reboot_index];
 
-        for z in instruction.z_range.clone() {
-            for y in instruction.y_range.clone() {
-                for x in instruction.x_range.clone() {
+        for z in cuboid.z_range.clone() {
+            for y in cuboid.y_range.clone() {
+                for x in cuboid.x_range.clone() {
                     self.cubes
                         .entry((x, y, z))
-                        .and_modify(|status| *status = instruction.status)
-                        .or_insert(instruction.status);
+                        .and_modify(|status| *status = cuboid.status)
+                        .or_insert(cuboid.status);
                 }
             }
         }
@@ -137,13 +312,19 @@ impl Reactor {
 fn solve_part1(inputfile: String) -> usize {
     let mut reactor = Reactor::from_file(&inputfile);
 
-    reactor.limited_boot();
-    reactor.num_cubes()
+    //reactor.limited_boot();
+    //reactor.num_cubes()
+    let active_cuboids = reactor.split_cuboids(true);
+    Reactor::num_cubes_in_cuboids(&active_cuboids)
 }
 
 fn solve_part2(inputfile: String) -> usize {
-    0
+    let mut reactor = Reactor::from_file(&inputfile);
+
+    let active_cuboids = reactor.split_cuboids(false);
+    Reactor::num_cubes_in_cuboids(&active_cuboids)
 }
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Part1: {}", solve_part1(args[1].to_string()));
@@ -154,6 +335,21 @@ fn main() {
 mod tests {
     use super::*;
     #[test]
+    fn test_split_2d_cuboids() {
+        let mut reactor = Reactor::from_string(
+            "on x=10..12,y=10..12,z=10..10
+on x=11..13,y=11..13,z=10..10",
+        );
+
+        assert_eq!(reactor.num_cuboids(), 2);
+        let active_cuboids = reactor.split_cuboids(false);
+        Reactor::print(&active_cuboids);
+        assert_eq!(active_cuboids.len(), 3);
+
+        assert_eq!(Reactor::num_cubes_in_cuboids(&active_cuboids), 14);
+    }
+
+    #[test]
     fn test_split_cuboids() {
         let mut reactor = Reactor::from_string(
             "on x=10..12,y=10..12,z=10..12
@@ -162,9 +358,13 @@ off x=9..11,y=9..11,z=9..11
 on x=10..10,y=10..10,z=10..10",
         );
 
-        reactor.split_cuboids();
+        let active_cuboids = reactor.split_cuboids(false);
 
-        assert_eq!(reactor.num_cubes_in_cuboids(), 27 + 19 - 8 + 1);
+        Reactor::print(&active_cuboids);
+        assert_eq!(
+            Reactor::num_cubes_in_cuboids(&active_cuboids),
+            27 + 19 - 8 + 1
+        );
     }
 
     #[test]
@@ -202,6 +402,7 @@ on x=10..10,y=10..10,z=10..10",
         assert_eq!(reactor.num_cubes(), 27 + 19 - 8 + 1);
     }
 
+    /*
     #[test]
     fn test_larger_example() {
         let mut reactor = Reactor::from_string(
@@ -232,4 +433,5 @@ on x=10..10,y=10..10,z=10..10",
         reactor.limited_boot();
         assert_eq!(reactor.num_cubes(), 590784);
     }
+    */
 }
